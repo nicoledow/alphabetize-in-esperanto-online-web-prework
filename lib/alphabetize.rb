71cd6ab43acde3d148504f31b50fb34b8e944f8a
Nicole Dow <nikki.dow71@gmail.com>
2019-06-25 20:50:07 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,18 +1,13 @@
 require "pry"
 
 def alphabetize(arr)
-  #esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "ŭ", "v", "z"]
   
-
  arr.sort_by do |word|
   word.split('').map do |letter|
     esperanto_alphabet.index(letter)
   end
 end
-
-
-
 
   arr
 end
